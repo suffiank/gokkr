239ec9c02c7cf850c4c6a25d95609db1ebf1a2ff
suffian <khansn@ornl.gov>
2015-11-22 09:35:35 -0500

    gauss-legendre table working

--- src/util/src/main.cc
+++ src/util/src/main.cc
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 #include "loadkvp.h"
 #include "logger.h"
+#include "mathfn.h"
 
 int main(int argc, char **argv) {
 
@@ -10,8 +12,13 @@ int main(int argc, char **argv) {
   bool good = safeloadkvp("input.txt",&kvp);
   if( !good ) return -1;
 
-  int i = 100;
+  int n = 11;
+  vector<dble> x(n), w(n);
+  fill_gauss_legendre_table(n, x.data(), w.data());
+
   logger.enable("stdout");
   logger.focus("stdout");
-  logf("My int i=%i\n",i);
+  logf("%3s %20s%20s\n","i","x","w");
+  for(int i = 0; i < n; i++)
+    logf("%3i %20.15f%20.15f\n",i+1,x[i],w[i]);
 }
